efccc616606e1b8429dd05212ad824c363f848dd
Becky Hayling <becky.hayling@mayden.co.uk>
2021-05-04 11:34:15 +0100

    Reword help text

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
                 </button>
               </div>
               <div className="border-double border-4 border-indigo-200 rounded-md p-3 mt-6 xl:mx-96 lg:mx-64 sm:mx-24 mx-12">
-                <p className="">Choose the number of words you'd like, and press the button to randomly choose. You can also manually choose words, rearrange and delete them</p>
+                <p className="">Choose the number of words you'd like, then press the button to randomly generate. You can also manually select words below, and drag to rearrange/delete them.</p>
               </div>
             </div>
             <div className="flex flex-row justify-center content-center text-base leading-relaxed text-gray-400">
